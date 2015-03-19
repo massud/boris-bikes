2e1dfbb77add84d85e1d640a322d498b52d0ad38
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-03-19 17:38:16 +0000

    eighth commit

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,15 @@
+require 'docking_station'
+describe DockingStation do
+  it { is_expected.to respond_to :release_bike }
+  it 'releases bikes that are not broken' do
+    subject.dock double :bike, broken?: false
+    bike = subject.release_bike
+    expect(bike).not_to be_broken
+  end
+  it 'can dock a bike' do
+    expect(subject.dock :bike).to eq :bike
+  end
+  it 'raises error when no bikes available' do
+    expect { subject.release_bike }.to raise_error 'No Bikes Available'
+  end
+end
